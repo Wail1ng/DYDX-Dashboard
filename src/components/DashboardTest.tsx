3d1a85e3458ff2f9306577766b3ebb9c72e902f1
whaling <inuksuk@whaling.xyz>
2024-09-30 23:51:57 +0200

    test push

---
+++ src/components/DashboardTest.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+import BalanceCard from "./BalanceCard";
+import StakingCard from "./StakingCard";
+import RewardsCard from "./AllBalanceCard";
+import UnbondingCard from "./UnbondingCard";
+import { DelegatorCard } from "./DelegatorCard";
+import DelegationCard from "./DelegationCard";
+import AllBalanceCard from "./AllBalanceCard";
+
+const Dashboard = ({ address }: { address: string }) => {
+  return (
+    <div>
+      <StakingCard address={address} />
+      <BalanceCard address={address} />
+      <RewardsCard delegator_address={address} />
+      <UnbondingCard delegator_address={address} />
+      <DelegatorCard delegator_address={address} />
+      <DelegationCard delegator_address={address} />
+      <AllBalanceCard delegator_address={address} />
+    </div>
+  );
+};
+
+export default Dashboard;
