acce2a44326ab6ec913fba8114667a48cb1a9b98
Gauthier <gauthier.axel.lory@gmail.com>
2024-10-20 19:21:26 +0200

    Auto stash before merge of "main" and "origin/main"

--- src/components/RewardsCard.tsx
+++ src/components/RewardsCard.tsx
@@ -3,7 +3,8 @@ import { getRewardsData } from "@/services/dydx"
 import { Card, CardContent, CardHeader, CardTitle } from "./ui/card";
 
 export async function RewardsCard({ address }: { address: string }) {
-const data = await getRewardsData(address);
+  const data = await getRewardsData(address);
+
 
   return (
     <Card>
@@ -11,7 +12,7 @@ const data = await getRewardsData(address);
         <CardTitle>Claimable Rewards</CardTitle>
       </CardHeader>
       <CardContent>
-      <p>{data?.total?.find(item => item.denom === "ibc/8E27BA2D5493AF5636760E354E46004562C46AB7EC0CC4C1CA14E9E20E2545B5")?.amount}</p>
+        <p>{data?.total?.find(item => item.denom === "ibc/8E27BA2D5493AF5636760E354E46004562C46AB7EC0CC4C1CA14E9E20E2545B5")?.amount}</p>
       </CardContent>
     </Card>
   );
